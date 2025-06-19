3d800422382f2dc6e23c5abd03f1e43b240be842
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2025-06-19 15:30:16 +0200

    Added LoadinSpiner component and used it in appointmentSUmmary and appointment Page

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+import styles from "@/styles/components/LoadingSpinner.module.scss";
+
+function LoadingSpinner() {
+  return <div className={styles.spinner}></div>;
+}
+
+export default LoadingSpinner;
